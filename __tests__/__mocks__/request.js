e4bcc5d46d6d39aebd6cc85f868979ecafa99613
davidkelley <david.james.kelley@gmail.com>
2017-02-20 12:37:09 +0000

    changing uri to url

--- __tests__/__mocks__/request.js
+++ __tests__/__mocks__/request.js
@@ -1,6 +1,6 @@
 export default function request(obj, cb) {
   process.nextTick(() => {
-    const err = obj.uri.match(/error/i) ? true : false;
+    const err = obj.url.match(/error/i) ? true : false;
     cb(err, { statusCode: 200 }, {})
   })
 }
